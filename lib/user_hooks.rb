66dd707826a8fc546ceeb5b1398256092ac436ac
Mattia Rossi <mattia.rossi@gmail.com>
2013-11-26 12:29:29 +0100

    Remove safe_attributes reference

--- lib/user_hooks.rb
+++ lib/user_hooks.rb
@@ -1,13 +1,11 @@
 class User < Principal
   has_one :xmpp_setting, :dependent => :destroy, :class_name => "XmppNotificationsUserSetting"
 
-  safe_attributes 'xmpp_jid'
-  
   def xmpp_jid
     return "" unless self.xmpp_setting
     return self.xmpp_setting.jid
   end
-  
+
   def xmpp_jid=(jid)
     self.xmpp_setting = XmppNotificationsUserSetting.new unless self.xmpp_setting
     self.xmpp_setting.jid = jid
