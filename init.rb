fa5b462f6012b6e9407c58161695fb3f00d48c48
Mattia Rossi <mattia.rossi@gmail.com>
2013-11-26 12:30:21 +0100

    Add safe_attributes handling

--- init.rb
+++ init.rb
@@ -1,13 +1,17 @@
 require "redmine"
-
-# RAILS_DEFAULT_LOGGER.info 'Starting XMPP Notifications Plugin for Redmine'
-
 require "rubygems"
 require "xmpp4r-simple"
 
 require_dependency "notifier_hook"
 require_dependency "my_account_hooks"
 require_dependency "user_hooks"
+require_dependency "user"
+
+if User.const_defined? "SAFE_ATTRIBUTES"
+    User::SAFE_ATTRIBUTES << "xmpp_jid"
+else
+    User.safe_attributes "xmpp_jid"
+end
 
 Redmine::Plugin.register :redmine_xmpp_notifications do
   name "Redmine XMPP Notifications plugin"
