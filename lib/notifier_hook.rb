f5c2692e6189dfb3813f0e5860cc7f5585c0dbf7
cmrd Senya <senya@riseup.net>
2017-10-05 00:24:49 +0300

    Implement notifications for wikis and boards
    Besides this makes a slight refactoring to XmmpNotificationView in order to
    make it a base class for any kind of XMPP notifications we might want.

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -6,7 +6,7 @@ class NotifierHook < Redmine::Hook::Listener
     issue = context[:issue]
 
     deliver(issue) do |user|
-      XmppNotificationRenderer.new(context, user).render_issue
+      IssueNotificationView.new(context, user).render_issue
     end
   end
 
@@ -14,9 +14,27 @@ class NotifierHook < Redmine::Hook::Listener
     journal = context[:journal]
 
     deliver(journal) do |user|
-      XmppNotificationRenderer.new(context, user).render_journal
+      IssueNotificationView.new(context, user).render_journal
     end
   end
+
+  def controller_wiki_edit_after_save(context={})
+    page = context[:page]
+
+    deliver(page.content) do |user|
+      WikiNotificationView.new(context, user).render
+    end
+  end
+
+  def controller_messages_new_after_save(context={})
+    message = context[:message]
+
+    deliver(message) do |user|
+      MessageNotificationView.new(context, user).render
+    end
+  end
+
+  alias_method :controller_messages_reply_after_save, :controller_messages_new_after_save
 
   private
 
@@ -36,7 +54,7 @@ class NotifierHook < Redmine::Hook::Listener
 
   def notification_recipients(object)
     notification_recipients = object.notified_users
-    notification_recipients += object.notified_watchers if config["send_to_watchers"]
+    notification_recipients += fetch_watchers(object) if config["send_to_watchers"]
     notification_recipients.uniq!
     notification_recipients.keep_if {|user| user.xmpp_jid.present? }
     if notification_recipients.any?
@@ -44,6 +62,20 @@ class NotifierHook < Redmine::Hook::Listener
       notification_recipients.delete(author) if author.logged? && author.pref.no_self_notified
     end
     notification_recipients
+  end
+
+  def fetch_watchers(object)
+    (object.try(:notified_watchers) || object.page.notified_watchers)
+      .concat(message_watchers(object))
+  end
+
+  def message_watchers(message)
+    return [] unless message.respond_to?(:parent)
+    if message.parent.nil?
+      message.try(:board).try(:notified_watchers) || []
+    else
+      message.parent.try(:notified_watchers) || []
+    end
   end
 
   def config
